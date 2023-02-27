b7a1fd81f8c64ef5c9eb89b35c68df64b4e2baa0
Said Eslander <saideslander@gmail.com>
2023-02-27 11:23:53 -0500

    configuracion de whitenoise agregando staticfiles por el comando colletion

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,17 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR/"staticfiles"
 #carpeta estilos
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     #os.path.join(BASE_DIR, 'myapp/static'),
 ]
-
-STATIC_ROOT = BASE_DIR/"staticfiles"
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
